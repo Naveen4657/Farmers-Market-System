822abfeef5297e1954efa6d44f4ec044c51435a8
Naveen4657 <122519044+Naveen4657@users.noreply.github.com>
2025-10-04 15:04:20 +0530

    Create OrderItem.java

---
+++ OrderItem.java
@@ -0,0 +1,26 @@
+package farmersmarket;
+
+public class OrderItem {
+    private Product product;
+    private int quantity;
+
+    public OrderItem(Product product, int quantity) {
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Product getProduct() {
+    	return product;
+    	}
+    public int getQuantity() {
+    	return quantity; 
+    	}
+    public double getSubtotal() {
+    	return product.getPrice() * quantity;
+    	}
+
+    @Override
+    public String toString() {
+        return product.getName() + " x " + quantity + " = " + getSubtotal();
+    }
+}
